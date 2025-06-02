2e54b43a268a913fe5ec40dc904ce391038dbfb3
pedromalta14 <rm95182@estudante.fieb.edu.br>
2025-06-02 12:12:52 +0000

    Grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,14 @@
 import poke from './assets/pokeapi_256 2.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
+import PokeFlex from './Pokeflex'
+import PokeGrid from './PokeGrid'
 
 const Home = ()=>{
     return (
         <div>
-            <img src= {poke} />
-            <img src={eevee} />           
-             <img src= {poke} />
-            <img src={eevee} />    
-             <img src= {poke} />
-            <img src={eevee} />    
+           <PokeFlex />
+           <PokeGrid />
              
         </div>
     )
---
+++ src/PokeGrid.jsx
@@ -0,0 +1,19 @@
+import poke from './assets/pokeapi_256 2.png'
+import eevee from './assets/eevee.jpg'
+import './PokeGrid.css'
+
+const PokeGrid = ()=>{
+    return (
+        <div className="PokeGrid">
+            <img src= {poke} />
+            <img src={eevee} />           
+             <img src= {poke} />
+            <img src={eevee} />    
+             <img src= {poke} />
+            <img src={eevee} />    
+             
+        </div>
+    )
+}
+
+export default PokeGrid
